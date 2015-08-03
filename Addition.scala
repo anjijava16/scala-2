e19b1de9938caeb545ed6dd985fa8db72f2bcb32
sreejith <sreejithc321@gmail.com>
2015-08-03 18:44:40 +0530

    function

---
+++ Addition.scala
@@ -0,0 +1,13 @@
+
+object Addition
+	{    
+		def main(args : Array[String])       
+			{        
+				println ("sum = " + sumOf(100,20))    
+			}       
+	
+		def sumOf(a:Int,b:Int): Int =    
+			{        
+				return a + b    
+			}
+	}
