fc60399661fb81de586706963b53e5120b955c6e
sreejith <sreejithc321@gmail.com>
2015-08-03 18:23:06 +0530

    hello world program

---
+++ HelloWorld.scala
@@ -0,0 +1,16 @@
+object HelloWorld
+    { 
+        /*  This is an example of multi-line comments
+          * Scala program to print 'Hello World' as the output   
+          * For class names, the first letter should be in Upper Case
+        */  
+
+        def main(args: Array[String])
+             {        
+                  /* All method names should start with a Lower Case letter
+                   * Scala program processing starts from the main()
+                   * Name of the program file should exactly match the object name.
+                  */
+                  println("Hello, world!")   
+             }
+     } 
