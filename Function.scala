fdb9f7dd532c771336bef9e4fbce7d4bb6edeb03
sreejith <sreejithc321@gmail.com>
2015-08-05 10:20:19 +0530

    function

--- Function.scala
+++ Function.scala
@@ -1,13 +1,13 @@
 
-object function
-	{    
-		def main(args : Array[String])       
-			{        
-				println ("sum = " + sumOf(100,20))    
-			}       
-	
-		def sumOf(a:Int,b:Int): Int =    
-			{        
-				return a + b    
-			}
-	}
+object Function
+{    
+	def main(args : Array[String])       
+		{        
+			println ("sum = " + sumOf(100,20))    
+		}       
+
+	def sumOf(a:Int,b:Int): Int =    
+		{        
+			return a + b    
+		}
+}
